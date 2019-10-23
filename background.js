0254006aaf21fae1507b8bbe7ae5a06676fade18
Ian Pan <g-hamu@hotmail.com>
2019-10-23 17:57:42 +0800

    Change page action to browser action cause our extension should apply to all kind of websites

--- background.js
+++ background.js
@@ -27,17 +27,3 @@ function initEnvOnInstall() {
     chrome.storage.sync.set({isMarked: false}, null);
   });
 }
-
-chrome.runtime.onInstalled.addListener(function() {
-  chrome.storage.sync.set({color: '#3aa757'}, function() {
-    console.log('The color is green.');
-  });
-  chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
-    chrome.declarativeContent.onPageChanged.addRules([{
-      conditions: [new chrome.declarativeContent.PageStateMatcher({
-        pageUrl: {hostEquals: 'developer.chrome.com'},
-      })],
-      actions: [new chrome.declarativeContent.ShowPageAction()]
-    }]);
-  });
-});
