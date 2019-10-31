a5815350f21ed7dc790fa26931f561d5fae69152
Ian Pan <g-hamu@hotmail.com>
2019-10-31 19:22:05 +0800

    Use shortcut command to trigger popup display

--- ContentScripts/content.js
+++ ContentScripts/content.js
@@ -1,11 +1,5 @@
 'use strict';
 
-chrome.extension.onMessage.addListener(function(msg) {
-    if (msg.action === 'toggle_highlight') {
-        toggleHighlight(msg.isMarked);
-    }
-});
-
 chrome.runtime.onMessage.addListener(function(msg) {
     if(msg.action === "highlight"){
         let isInputTextAttribute = (msg.targetAttr !==  undefined);
